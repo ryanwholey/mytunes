8b2c5340d6df3a1feff86543ea8ae9a34c406952
naomiajacobs <naomiajacobs@gmail.com>
2015-09-03 08:29:07 -0700

    Moved stuff from app view to app model to make code cleaner

--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -3,7 +3,7 @@ var AppModel = Backbone.Model.extend({
 
   initialize: function(params) {
     this.set('currentSong', new SongModel());
-    this.set('songQueue', new SongQueue());
+    // this.set('songQueue', new SongQueue());
 
     /* Note that 'this' is passed as the third argument. That third argument is
     the context. The 'play' handler will always be bound to that context we pass in.
@@ -14,7 +14,16 @@ var AppModel = Backbone.Model.extend({
 
 
     params.library.on('play', function(song) {
+      console.log('PLAYING');
       this.set('currentSong', song);
+    }, this);
+
+    params.songQueue.on('ending', function() {
+      console.log('I EXIST');
+    }, this);
+
+    params.songQueue.on('enqueue', function(song){
+      params.songQueue.add(song);
     }, this);
 
     // params.library.on('enqueue', function(song) {
